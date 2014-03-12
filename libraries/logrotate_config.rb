47cb52e5abafa57d09c4999c6f6632008fe4fc57
Fernando Honig <fernando.honig@rackspace.co.uk>
2014-03-12 16:23:56 +0000

    Resolving rubocop offences

--- libraries/logrotate_config.rb
+++ libraries/logrotate_config.rb
@@ -17,7 +17,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+# Module CookbookLogrotate
 module CookbookLogrotate
   DIRECTIVES ||= %w[
     compress        copy        copytruncate    daily           dateext
